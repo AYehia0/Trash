c2fa21ffcd8c3ea9a460ea928a0e77679258e7b4
AYehia0 <ahmedyehia611@gmail.com>
2023-09-10 16:20:50 +0200

    [PARAER]: If else statements

--- ast/ast.go
+++ ast/ast.go
@@ -186,3 +186,42 @@ func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string {
 	return b.Token.Literal
 }
+
+// the block is basically many statements contained by {}
+type BlockStatement struct {
+	Token      token.Token // {
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+// if (<condition>) <consequence> else <alternative>
+type IfExpression struct {
+	Token       token.Token // the if token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+	return out.String()
+}
--- parser/parser.go
+++ parser/parser.go
@@ -89,6 +89,9 @@ func New(lexer *lexer.Lexer) *Parser {
 	// we only need to parse the left pren !!!
 	p.registerPrefix(token.LEFT_PAREN, p.parseGroupedExpression)
 
+	// other keywords if, else... etc
+	p.registerPrefix(token.IF, p.parseIfExpression)
+
 	// read 2 tokens so current and next token are set
 	p.nextToken()
 	p.nextToken()
@@ -323,4 +326,60 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 		return nil
 	}
 	return exp
+}
+func (p *Parser) parseIfExpression() ast.Expression {
+	exp := &ast.IfExpression{
+		Token: p.currToken,
+	}
+
+	if !p.expectNextToken(token.LEFT_PAREN) {
+		return nil
+	}
+
+	p.nextToken()
+	exp.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectNextToken(token.RIGHT_PAREN) {
+		return nil
+	}
+
+	if !p.expectNextToken(token.LEFT_BRACE) {
+		return nil
+	}
+
+	exp.Consequence = p.parseBlockStatement()
+
+	if p.TokenIs(p.peekToken, token.ELSE) {
+		p.nextToken()
+
+		if !p.expectNextToken(token.LEFT_BRACE) {
+			return nil
+		}
+
+		exp.Alternative = p.parseBlockStatement()
+	}
+
+	return exp
+}
+
+// TODO: implement the block statement here
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := ast.BlockStatement{
+		Token: p.currToken,
+	}
+
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.TokenIs(p.currToken, token.RIGHT_BRACE) && !p.TokenIs(p.currToken, token.EOF) {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return &block
 }
