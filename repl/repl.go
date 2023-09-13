34beb3182457ad67476d3e9514e7088fbbfb8203
AYehia0 <ahmedyehia611@gmail.com>
2023-09-13 12:05:08 +0000

    [REPL]: giving trash some shape

--- repl/repl.go
+++ repl/repl.go
@@ -9,13 +9,23 @@ import (
 	"fmt"
 	"io"
 	"trash/lexer"
-	"trash/token"
+	"trash/parser"
 )
+
+const TRASH_ICON = `
+ _                 _     
+| |               | |			___^___
+| |_ _ __ __ _ ___| |__	               |-------|
+| __| '__/ _| / __| '_ \		| | | |
+| |_| | | (_| \__ \ | | |		| | | |
+ \__|_|  \__,_|___/_| |_|		|_____|
+`
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	fmt.Print(TRASH_ICON)
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -24,8 +34,22 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for t := l.NextToken(); t.Type != token.EOF; t = l.NextToken() {
-			fmt.Printf("%+v\n", t)
+
+		parser := parser.New(l)
+		prog := parser.Parse()
+		if len(parser.Errors()) != 0 {
+			logErrors(out, parser.Errors())
+			continue
 		}
+		io.WriteString(out, prog.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func logErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Oops errors :'( \n")
+	io.WriteString(out, "Parser Errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
