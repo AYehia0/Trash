97173da7d7a07fd5130f6980da22644750196343
AYehia0 <ahmedyehia611@gmail.com>
2023-11-12 15:32:23 +0200

    Eval: Take input from file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/user"
@@ -10,11 +11,27 @@ import (
 const INTER_NAME = "Trash"
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		fmt.Printf("Error : %s ", err.Error())
+	args := os.Args
+
+	if len(args) == 2 {
+		filePath := args[1]
+		// Reading from a file
+		file, err := os.Open(filePath)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		repl.StartWithFile(bufio.NewReader(file), os.Stdout)
+	} else if len(args) == 1 {
+
+		user, err := user.Current()
+		if err != nil {
+			fmt.Printf("Error : %s ", err.Error())
+		}
+		fmt.Printf("Hi %s!, Ever heard of %s ?\n", user.Username, INTER_NAME)
+		fmt.Printf("Type something in %s\n", INTER_NAME)
+		repl.Start(os.Stdin, os.Stdout)
 	}
-	fmt.Printf("Hi %s!, Ever heard of %s ?\n", user.Username, INTER_NAME)
-	fmt.Printf("Type something in %s\n", INTER_NAME)
-	repl.Start(os.Stdin, os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -8,6 +8,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"strings"
 	"trash/eval"
 	"trash/lexer"
 	"trash/object"
@@ -21,6 +23,7 @@ const TRASH_ICON = `
 | __| '__/ _| / __| '_ \		| | | |
 | |_| | | (_| \__ \ | | |		| | | |
  \__|_|  \__,_|___/_| |_|		|_____|
+
 `
 
 const PROMPT = ">> "
@@ -60,4 +63,48 @@ func logErrors(out io.Writer, errors []string) {
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
+}
+
+func StartWithFile(input io.Reader, output io.Writer) {
+	env := object.NewEnv()
+	content, err := ioutil.ReadAll(input)
+	if err != nil {
+		fmt.Fprintln(output, "Error reading the file:", err)
+		return
+	}
+
+	// Convert the content to a string
+	codeBlock := string(content)
+
+	// Parse and evaluate the code block
+	l := lexer.New(codeBlock)
+	p := parser.New(l)
+	program := p.Parse()
+
+	if len(p.Errors()) != 0 {
+		logErrors(output, p.Errors())
+	} else {
+		evaluated := eval.Eval(program, env)
+		if evaluated != nil {
+			fmt.Fprintln(output, evaluated.Inspect())
+		}
+	}
+}
+func IsStartOfBlock(line string) bool {
+	line = strings.TrimSpace(line)
+	if len(line) == 0 {
+		return false // Handle empty lines
+	}
+	lastChar := line[len(line)-1]
+	return lastChar == '{'
+}
+
+// IsEndOfBlock checks if a line marks the end of a code block.
+func IsEndOfBlock(line string) bool {
+	line = strings.TrimSpace(line)
+	if len(line) == 0 {
+		return false // Handle empty lines
+	}
+	lastChar := line[len(line)-1]
+	return lastChar == '}'
 }
